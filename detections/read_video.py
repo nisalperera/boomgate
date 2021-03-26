64a96303271fd6a4d42e585f2f520dae97535281
Nisal Perera <nisalc@orelit.com>
2021-03-26 17:07:27 +0530

    almost working

--- detections/read_video.py
+++ detections/read_video.py
@@ -1,35 +1,157 @@
 import cv2
-from detections.vehicle_detection import detection
+import logging
 import time
+import numpy as np
+from datetime import datetime
+from detections.vehicle_detection import detection
+from database.dbconnection import MongoDBConnection
+from database.model import VehicleDetails
 
 
-def select_best(v_confs, p_confs, ocr_confs):
-    print()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+skip_frames = 5
+
+con = MongoDBConnection("localhost:27017", "boomgate")
+mongoclient = con.connect()
+
+vehicle_image_path = "./images/vehicles/{name}"
+license_image_path = "./images/license_plates/{name}"
+
+# print(mongoclient.server_info())
+
+
+def read_image():
+    image = cv2.imread("./data_utils/images/frame137.jpg")
+    image = cv2.resize(image, None, fx=0.4, fy=0.4)
+    original, vehicle_confidence = detection(image)
+    cv2.imshow("Image", original)
+    print("Detection: ", vehicle_confidence)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def saveToDB(data, date_time, file_name):
+    vehicledetails = VehicleDetails()
+    vehicledetails.date_time = date_time
+    vehicledetails.vehicle_type = data[0][0]
+    # logger.error("Error: {}".format(data))
+    if data[2][0] is None or len(data[2][0]) == 0:
+        vehicledetails.license_plate = ""
+    elif len(data[2][0]) > 1:
+        string = ""
+        for s in data[2][0]:
+            string += s
+        vehicledetails.license_plate = string
+    else:
+        vehicledetails.license_plate = data[2][0]
+    vehicledetails.incoming = data[0][2]
+    vehicledetails.vehicle_image_path = "./images/vehicles/{name}".format(name=file_name)
+    vehicledetails.plate_image_path = "./images/license_plates/{name}".format(name=file_name)
+    con.saveToDb(vehicledetails)
+
+
+def select_best(confidences):
+    max_confidence_index = 0
+    for i, confidence_list in enumerate(confidences):
+        max_confidence = 0
+        if len(confidence_list) > 0 and len(confidence_list[2]) > 0 and confidence_list[2][1] > max_confidence and len(confidence_list[2][0]) > 0:
+            max_confidence = confidence_list[2][1]
+            max_confidence_index = i
+        else:
+            continue
+
+    return confidences[max_confidence_index], max_confidence_index
+    # if len(confidences[:, 2]) > 0:
+    #     licence_plate_confidences += confidences[:, 2, 1]
 
 
 def read_video():
     # cap = cv2.VideoCapture("./data_utils/video_files/6.mp4")
-    cap = cv2.VideoCapture("./data_utils/video_files/cctv_2.mp4")
-    print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    ret = True
-    while ret:
+    cap = cv2.VideoCapture("./data_utils/video_files/6.mp4")
+    # print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_count = 0
+
+    confidence_list = []
+    frame_list = []
+    license_plate_list = [None]
+    prev_license_plate = None
+    save = True
+
+    while True:
         start_time = time.time()
         ret, frame = cap.read()
+
+        if not ret:
+            break
+
         height, width, channels = frame.shape
-        print("Height: {h}, Width: {w}".format(h=height, w=width))
-        image = cv2.resize(frame, None, fx=0.4, fy=0.4)[int(height*0.093):, int(width*0.028):int(width*0.140)]
-        # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, 110:540]
-        # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, :]
-        # image = cv2.resize(frame, None, fx=0.4, fy=0.4)
-        original, vehicle_confidence, ocr_confidence, plate_confidence = detection(image)
+        # print("Height: {h}, Width: {w}".format(h=height, w=width))
+        frame_count += 1
+        image = cv2.resize(frame, None, fx=0.4, fy=0.4)
+        # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[int(height*0.093):, int(width*0.028):int(width*0.140)]
 
-        print("Vehicle confidence: ", vehicle_confidence)
-        print("Plate confidence: ", plate_confidence)
-        print("OCR confidence: ", ocr_confidence)
-        cv2.imshow("Image", original)
+        if skip_frames > 0:
+            if frame_count % skip_frames == 0:
+                # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, 110:540]
+                # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, :]
+                # image = cv2.resize(frame, None, fx=0.4, fy=0.4)
+                original, license_plate, vehicle_confidence, stop_detection = detection(image)
+                confidence_list.append(vehicle_confidence)
 
-        print("FPS: ", 1/(time.time() - start_time))
-        print("Time to execute: ", time.time() - start_time)
+                frame_list.append(original)
+                if license_plate is None:
+                    if license_plate_list[-1] is not None:
+                        license_plate_list.append(license_plate_list[-1])
+                    else:
+                        license_plate_list.append(None)
+                else:
+                    license_plate_list.append(license_plate)
+                if stop_detection:
+                    logger.info("Selecting best predictions.")
+                    max_confidences, index = select_best(confidence_list)
+                    if save:
+                        logger.info("Max Vehicle confidence: {conf}".format(conf=max_confidences))
+                        logger.info("Saving data to Database")
+                        date_time = datetime.now()
+                        file_name = max_confidences[0][0] + str(date_time) + ".jpg"
+                        saveToDB(max_confidences, date_time, file_name)
+                        cv2.imwrite(vehicle_image_path.format(name=file_name), frame_list[index])
+                        if license_plate_list[index] is not None:
+                            cv2.imwrite(license_image_path.format(name=file_name), license_plate_list[index])
+                        else:
+                            cv2.imwrite(license_image_path.format(name=file_name), np.zeros(frame_list[index].shape))
+                        save = False
+                else:
+                    # print("Vehicle confidence: ", confidence_list)
+                    save = True
+                    # print("Plate confidence: ", plate_confidence)
+                    # print("OCR confidence: ", ocr_confidence)
+
+                # if license_plate is not None and license_plate.shape[0] > 0 and license_plate.shape[1] > 0:
+                #     cv2.imshow("Image", original)
+                # else:
+                cv2.imshow("Image", original)
+
+                logger.info("FPS: {fps}".format(fps=skip_frames/(time.time() - start_time)))
+                logger.info("Time to execute: {exect}".format(exect=time.time() - start_time))
+        else:
+            # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, 110:540]
+            # image = cv2.resize(frame, None, fx=0.4, fy=0.4)[200:, :]
+            # image = cv2.resize(frame, None, fx=0.4, fy=0.4)
+            original, license_plate, vehicle_confidence = detection(image)
+
+            print("Vehicle confidence: ", vehicle_confidence)
+            # print("Plate confidence: ", plate_confidence)
+            # print("OCR confidence: ", ocr_confidence)
+            if license_plate is not None and license_plate.shape[0] > 0 and license_plate.shape[1] > 0:
+                cv2.imshow("Image", original)
+            else:
+                cv2.imshow("Image", original)
+
+            print("FPS: ", 1 / (time.time() - start_time))
+            print("Time to execute: ", time.time() - start_time)
         key = cv2.waitKey(1)
         if key == 27:
             break
--- detections/vehicle_detection.py
+++ detections/vehicle_detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from detections import license_plates_detection
+from detections.tracking import ObjectTracker
 
 
 vehicle_net = cv2.dnn.readNet("./data_utils/weight_files/yolov4.weights","./data_utils/cfg_files/yolov4.cfg")
@@ -10,8 +11,14 @@ classes = []
 with open("./data_utils/classes/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
-vehicle_layer_names = vehicle_net.getLayerNames()
-vehicle_output_layers = [vehicle_layer_names[i[0] - 1] for i in vehicle_net.getUnconnectedOutLayers()]
+vehicle_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+vehicle_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv2.dnn_DetectionModel(vehicle_net)
+model.setInputParams(size=(416, 416), scale=1/255)
+
+# vehicle_layer_names = vehicle_net.getLayerNames()
+# vehicle_output_layers = [vehicle_layer_names[i[0] - 1] for i in vehicle_net.getUnconnectedOutLayers()]
 allowed_classes = ["car", "bus", "truck", "three_wheel"]
 color = (0, 0, 255)
 font = cv2.FONT_HERSHEY_PLAIN
@@ -19,79 +26,99 @@ font = cv2.FONT_HERSHEY_PLAIN
 bbox_list = []
 threshold_line = 200
 
+tracker = ObjectTracker(100)
+
 
 def moving_side(new, bbox):
     if new:
         bbox_list.clear()
-        return ""
+        return None
     elif len(bbox_list) == 0:
         bbox_list.append(bbox)
-        return ""
+        return None
     else:
         prev_bbox = bbox_list[-1]
-        if prev_bbox[1] > bbox[1] and prev_bbox[1]:
-            return "incoming"
+        if prev_bbox[1] > bbox[1]:
+            return True
         elif prev_bbox[0] < bbox[0]:
-            return "outgoing"
+            return False
         bbox_list.append(bbox)
 
 
 def detection(image):
     height, width, channel = image.shape
-    vehicle_blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    vehicle_net.setInput(vehicle_blob)
-    vehicles_outs = vehicle_net.forward(vehicle_output_layers)
+    # vehicle_blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    # vehicle_net.setInput(vehicle_blob)
+    # vehicles_outs = vehicle_net.forward(vehicle_output_layers)
 
-    class_ids = []
-    confidences = []
-    boxes = []
+    class_ids, confidences, boxes = model.detect(image, 0.9, 0.5)
+    class_ids_filtered, confidences_filtered, boxes_filtered = [], [], []
 
-    for out in vehicles_outs:
-        for detection in out:
-            # print(detection)
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
+    for i, class_id in enumerate(class_ids):
+        if classes[class_id[0]] in allowed_classes:
+            class_ids_filtered.append(class_id[0])
+            confidences_filtered.append(confidences[i])
+            boxes_filtered.append(boxes[i].tolist())
 
-            if confidence > 0.2 and not class_id == 0:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.85, 0.90)
+    # class_i-_car = class_ids.tolist().index(2)
 
     original_img = vehicle = image
-    plate_confidence = []
-    ocr_confidence = []
-    vehicle_confidence = []
+    # plate_confidence = []
+    # ocr_confidence = []
+    detections = []
+    # centroids = []
+    # calculate_max = False
 
-    for i in range(len(boxes)):
-        if i in indexes and str(classes[class_ids[i]]) in allowed_classes:
-            x1, y1, w1, h1 = boxes[i]
-            if y1 + int(h1/2) < threshold_line:
-                label = str(classes[class_ids[i]])
-                # print(label)
-                # vehicle = image[y+int(h/2):y+h, x:x+w]
-                if x1 > 0 and y1 > 0 and w1 > 0 and h1 > 0:
-                    vehicle = image[y1:y1+h1, x1:x1+w1]
-                vehicle_grayed = cv2.cvtColor(vehicle, cv2.COLOR_BGR2GRAY)
-                original_img, plate_confidence, ocr = license_plates_detection.detect_license_plate(vehicle_grayed, image, boxes[i])
-                side = moving_side(False, boxes[i][:2])
-                vehicle_confidence.append((label, confidences[i], side))
-                if ocr is not None:
-                    ocr_confidence.append(ocr)
-                cv2.rectangle(original_img, (x1, y1), (x1 + w1, y1 + h1), color, 1)
-                cv2.line(original_img, (0, threshold_line), (width, threshold_line), (255, 0, 0), 1)
-                cv2.putText(original_img, label, (x1, y1 - 5), font, 1, color, 1)
-                # print("Vehicle Y coodinate: ", y1 + (h1/2))
-            else:
-                pass
+    stop_detection = False
+    license_plate = None
 
-    return original_img, vehicle_confidence, ocr_confidence, plate_confidence
+    objects = tracker.update(np.array(boxes_filtered))
+    if objects is not None:
+        for (objectID, centroid) in objects.items():
+            # draw both the ID of the object and the centroid of the
+            # object on the output frame
+            bbox = centroid[2:].tolist()
+            if isinstance(centroid, np.ndarray) and isinstance(boxes_filtered, list) and any(box == bbox for box in boxes_filtered):
+                index = boxes_filtered.index(centroid[2:].tolist())
+                x1, y1, w1, h1 = boxes_filtered[index]
+                # if classes[class_ids[index][0]] in allowed_classes:
+                if centroid[1] < threshold_line:
+                    text = "ID {}".format(objectID)
+                    label = str(classes[class_ids_filtered[index]])
+
+                    if x1 > 0 and y1 > 0 and w1 > 0 and h1 > 0:
+                        vehicle = image[y1:y1 + h1, x1:x1 + w1]
+                    vehicle_grayed = cv2.cvtColor(vehicle, cv2.COLOR_BGR2GRAY)
+                    original_img, license_plate, plate_confidence, ocr = license_plates_detection.detect_license_plate(vehicle_grayed,
+                                                                                                        image,
+                                                                                                        boxes_filtered[index])
+                    side = moving_side(False, bbox)
+                    if ocr is None:
+                        # ocr_confidence.append(ocr)
+                        ocr = []
+                    else:
+                        pass
+
+                    # vehicle_confidence.append([(label, confidences[i], side), plate_confidence, ocr])
+                    detections += [[label, confidences_filtered[index][0], side], plate_confidence, ocr]
+
+                    cv2.rectangle(original_img, (x1, y1), (x1 + w1, y1 + h1), color, 1)
+                    cv2.line(original_img, (0, threshold_line), (width, threshold_line), (255, 0, 0), 1)
+                    cv2.putText(original_img, label, (x1, y1 - 5), font, 1, color, 1)
+                    cv2.putText(original_img, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    cv2.circle(original_img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
+                else:
+                    # if centroid[1] > threshold_line:
+                    # stop_detection = True
+                    side = moving_side(False, bbox)
+                    if not side and centroid[1] > threshold_line:
+                        stop_detection = True
+                    elif not side and centroid[1] < threshold_line:
+                        stop_detection = True
+                    elif side and centroid[1] < threshold_line:
+                        stop_detection = True
+                    break
+
+    return original_img, license_plate, detections, stop_detection
+    # return original_img
